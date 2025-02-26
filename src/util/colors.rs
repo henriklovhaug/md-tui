59f2df15c0597a25e216ce954363b824dfb6678b
Mike Krüger <mkrueger@posteo.de>
2025-02-26 19:11:15 +0100

    Color config is now configurable.

--- src/util/colors.rs
+++ src/util/colors.rs
@@ -1,10 +1,9 @@
 use std::str::FromStr;
 
 use config::{Config, Environment, File};
-use lazy_static::lazy_static;
 use ratatui::style::Color;
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct ColorConfig {
     // Inline styles
     pub italic_color: Color,
@@ -40,139 +39,165 @@ pub struct ColorConfig {
     pub quote_default: Color,
 }
 
-lazy_static! {
-    pub static ref COLOR_CONFIG: ColorConfig = {
-        let config_dir = dirs::home_dir().unwrap();
-        let config_file = config_dir.join(".config").join("mdt").join("config.toml");
-        let settings = Config::builder()
-            .add_source(File::with_name(config_file.to_str().unwrap()).required(false))
-            .add_source(Environment::with_prefix("MDT").separator("_"))
-            .build()
-            .unwrap();
+pub static mut COLOR_CONFIG: ColorConfig = ColorConfig {
+    heading_bg_color: Color::Blue,
+    heading_fg_color: Color::Black,
+    italic_color: Color::Reset,
+    bold_color: Color::Reset,
+    striketrough_color: Color::Reset,
+    quote_bg_color: Color::Reset,
+    code_fg_color: Color::Red,
+    code_bg_color: Color::Rgb(48, 48, 48),
+    code_block_bg_color: Color::Rgb(48, 48, 48),
+    link_color: Color::Blue,
+    link_selected_fg_color: Color::Green,
+    link_selected_bg_color: Color::DarkGray,
+    table_header_fg_color: Color::Yellow,
+    table_header_bg_color: Color::Reset,
+    file_tree_selected_fg_color: Color::LightGreen,
+    file_tree_page_count_color: Color::LightGreen,
+    file_tree_name_color: Color::Blue,
+    file_tree_path_color: Color::DarkGray,
+    bold_italic_color: Color::Reset,
+    quote_important: Color::LightRed,
+    quote_warning: Color::LightYellow,
+    quote_tip: Color::LightGreen,
+    quote_note: Color::LightBlue,
+    quote_caution: Color::LightMagenta,
+    quote_default: Color::White,
+};
 
-        ColorConfig {
-            heading_bg_color: Color::from_str(
-                &settings.get::<String>("h_bg_color").unwrap_or_default(),
-            )
+pub fn load_color_config() -> ColorConfig {
+    let config_dir = dirs::home_dir().unwrap();
+    let config_file = config_dir.join(".config").join("mdt").join("config.toml");
+    let settings = Config::builder()
+        .add_source(File::with_name(config_file.to_str().unwrap()).required(false))
+        .add_source(Environment::with_prefix("MDT").separator("_"))
+        .build()
+        .unwrap();
+
+    ColorConfig {
+        heading_bg_color: Color::from_str(
+            &settings.get::<String>("h_bg_color").unwrap_or_default(),
+        )
+        .unwrap_or(Color::Blue),
+        heading_fg_color: Color::from_str(
+            &settings.get::<String>("h_fg_color").unwrap_or_default(),
+        )
+        .unwrap_or(Color::Black),
+        italic_color: Color::from_str(
+            &settings.get::<String>("italic_color").unwrap_or_default(),
+        )
+        .unwrap_or(Color::Reset),
+        bold_color: Color::from_str(&settings.get::<String>("bold_color").unwrap_or_default())
+            .unwrap_or(Color::Reset),
+        striketrough_color: Color::from_str(
+            &settings
+                .get_string("striketrough_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::Reset),
+        quote_bg_color: Color::from_str(
+            &settings.get::<String>("quote_bg_color").unwrap_or_default(),
+        )
+        .unwrap_or(Color::Reset),
+        code_fg_color: Color::from_str(
+            &settings.get::<String>("code_fg_color").unwrap_or_default(),
+        )
+        .unwrap_or(Color::Red),
+        code_bg_color: Color::from_str(
+            &settings.get::<String>("code_bg_color").unwrap_or_default(),
+        )
+        .unwrap_or(Color::Rgb(48, 48, 48)),
+        code_block_bg_color: Color::from_str(
+            &settings
+                .get::<String>("code_block_bg_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::Rgb(48, 48, 48)),
+        link_color: Color::from_str(&settings.get::<String>("link_color").unwrap_or_default())
             .unwrap_or(Color::Blue),
-            heading_fg_color: Color::from_str(
-                &settings.get::<String>("h_fg_color").unwrap_or_default(),
-            )
-            .unwrap_or(Color::Black),
-            italic_color: Color::from_str(
-                &settings.get::<String>("italic_color").unwrap_or_default(),
-            )
-            .unwrap_or(Color::Reset),
-            bold_color: Color::from_str(&settings.get::<String>("bold_color").unwrap_or_default())
-                .unwrap_or(Color::Reset),
-            striketrough_color: Color::from_str(
-                &settings
-                    .get_string("striketrough_color")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::Reset),
-            quote_bg_color: Color::from_str(
-                &settings.get::<String>("quote_bg_color").unwrap_or_default(),
-            )
-            .unwrap_or(Color::Reset),
-            code_fg_color: Color::from_str(
-                &settings.get::<String>("code_fg_color").unwrap_or_default(),
-            )
-            .unwrap_or(Color::Red),
-            code_bg_color: Color::from_str(
-                &settings.get::<String>("code_bg_color").unwrap_or_default(),
-            )
-            .unwrap_or(Color::Rgb(48, 48, 48)),
-            code_block_bg_color: Color::from_str(
-                &settings
-                    .get::<String>("code_block_bg_color")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::Rgb(48, 48, 48)),
-            link_color: Color::from_str(&settings.get::<String>("link_color").unwrap_or_default())
-                .unwrap_or(Color::Blue),
-            link_selected_fg_color: Color::from_str(
-                &settings
-                    .get::<String>("link_selected_fg_color")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::Green),
-            link_selected_bg_color: Color::from_str(
-                &settings
-                    .get::<String>("link_selected_bg_color")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::DarkGray),
-            table_header_fg_color: Color::from_str(
-                &settings
-                    .get::<String>("table_header_fg_color")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::Yellow),
-            table_header_bg_color: Color::from_str(
-                &settings
-                    .get::<String>("table_header_bg_color")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::Reset),
-            file_tree_selected_fg_color: Color::from_str(
-                &settings
-                    .get::<String>("file_tree_selected_fg_color")
-                    .unwrap_or_default(),
-            )
+        link_selected_fg_color: Color::from_str(
+            &settings
+                .get::<String>("link_selected_fg_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::Green),
+        link_selected_bg_color: Color::from_str(
+            &settings
+                .get::<String>("link_selected_bg_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::DarkGray),
+        table_header_fg_color: Color::from_str(
+            &settings
+                .get::<String>("table_header_fg_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::Yellow),
+        table_header_bg_color: Color::from_str(
+            &settings
+                .get::<String>("table_header_bg_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::Reset),
+        file_tree_selected_fg_color: Color::from_str(
+            &settings
+                .get::<String>("file_tree_selected_fg_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::LightGreen),
+        file_tree_page_count_color: Color::from_str(
+            &settings
+                .get::<String>("file_tree_page_count_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::LightGreen),
+        file_tree_name_color: Color::from_str(
+            &settings
+                .get::<String>("file_tree_name_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::Blue),
+        file_tree_path_color: Color::from_str(
+            &settings
+                .get::<String>("file_tree_path_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::DarkGray),
+        bold_italic_color: Color::from_str(
+            &settings
+                .get::<String>("bold_italic_color")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::Reset),
+        quote_important: Color::from_str(
+            &settings
+                .get::<String>("quote_important")
+                .unwrap_or_default(),
+        )
+        .unwrap_or(Color::LightRed),
+        quote_warning: Color::from_str(
+            &settings.get::<String>("quote_warning").unwrap_or_default(),
+        )
+        .unwrap_or(Color::LightYellow),
+
+        quote_tip: Color::from_str(&settings.get::<String>("quote_tip").unwrap_or_default())
             .unwrap_or(Color::LightGreen),
-            file_tree_page_count_color: Color::from_str(
-                &settings
-                    .get::<String>("file_tree_page_count_color")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::LightGreen),
-            file_tree_name_color: Color::from_str(
-                &settings
-                    .get::<String>("file_tree_name_color")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::Blue),
-            file_tree_path_color: Color::from_str(
-                &settings
-                    .get::<String>("file_tree_path_color")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::DarkGray),
-            bold_italic_color: Color::from_str(
-                &settings
-                    .get::<String>("bold_italic_color")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::Reset),
-            quote_important: Color::from_str(
-                &settings
-                    .get::<String>("quote_important")
-                    .unwrap_or_default(),
-            )
-            .unwrap_or(Color::LightRed),
-            quote_warning: Color::from_str(
-                &settings.get::<String>("quote_warning").unwrap_or_default(),
-            )
-            .unwrap_or(Color::LightYellow),
 
-            quote_tip: Color::from_str(&settings.get::<String>("quote_tip").unwrap_or_default())
-                .unwrap_or(Color::LightGreen),
+        quote_note: Color::from_str(&settings.get::<String>("quote_note").unwrap_or_default())
+            .unwrap_or(Color::LightBlue),
 
-            quote_note: Color::from_str(&settings.get::<String>("quote_note").unwrap_or_default())
-                .unwrap_or(Color::LightBlue),
+        quote_caution: Color::from_str(
+            &settings.get::<String>("quote_caution").unwrap_or_default(),
+        )
+        .unwrap_or(Color::LightMagenta),
 
-            quote_caution: Color::from_str(
-                &settings.get::<String>("quote_caution").unwrap_or_default(),
-            )
-            .unwrap_or(Color::LightMagenta),
-
-            quote_default: Color::from_str(
-                &settings.get::<String>("quote_default").unwrap_or_default(),
-            )
-            .unwrap_or(Color::White),
-        }
-    };
+        quote_default: Color::from_str(
+            &settings.get::<String>("quote_default").unwrap_or_default(),
+        )
+        .unwrap_or(Color::White),
+    }
 }
 
 pub struct HeadingColors {
@@ -182,37 +207,42 @@ pub struct HeadingColors {
     pub level_5: Color,
     pub level_6: Color,
 }
+pub static mut HEADER_COLOR: HeadingColors = HeadingColors {
+    level_2: Color::Green,
+    level_3: Color::Magenta,
+    level_4: Color::Cyan,
+    level_5: Color::Yellow,
+    level_6: Color::LightRed,
+};
 
-lazy_static! {
-    pub static ref HEADER_COLOR: HeadingColors = {
-        let config_dir = dirs::home_dir().unwrap();
-        let config_file = config_dir.join(".config").join("mdt").join("config.toml");
-        let settings = Config::builder()
-            .add_source(File::with_name(config_file.to_str().unwrap()).required(false))
-            .build()
-            .unwrap();
+pub fn load_header_colors() -> HeadingColors {
+    let config_dir = dirs::home_dir().unwrap();
+    let config_file = config_dir.join(".config").join("mdt").join("config.toml");
+    let settings = Config::builder()
+        .add_source(File::with_name(config_file.to_str().unwrap()).required(false))
+        .build()
+        .unwrap();
 
-        HeadingColors {
-            level_2: settings
-                .get::<String>("h2_fg_color")
-                .map(|s| Color::from_str(&s).unwrap_or(Color::Green))
-                .unwrap_or(Color::Green),
-            level_3: settings
-                .get_string("h3_fg_color")
-                .map(|s| Color::from_str(&s).unwrap_or(Color::Magenta))
-                .unwrap_or(Color::Magenta),
-            level_4: settings
-                .get_string("h4_fg_color")
-                .map(|s| Color::from_str(&s).unwrap_or(Color::Cyan))
-                .unwrap_or(Color::Cyan),
-            level_5: settings
-                .get_string("h5_fg_color")
-                .map(|s| Color::from_str(&s).unwrap_or(Color::Yellow))
-                .unwrap_or(Color::Yellow),
-            level_6: settings
-                .get_string("h6_fg_color")
-                .map(|s| Color::from_str(&s).unwrap_or(Color::LightRed))
-                .unwrap_or(Color::LightRed),
-        }
-    };
+    HeadingColors {
+        level_2: settings
+            .get::<String>("h2_fg_color")
+            .map(|s| Color::from_str(&s).unwrap_or(Color::Green))
+            .unwrap_or(Color::Green),
+        level_3: settings
+            .get_string("h3_fg_color")
+            .map(|s| Color::from_str(&s).unwrap_or(Color::Magenta))
+            .unwrap_or(Color::Magenta),
+        level_4: settings
+            .get_string("h4_fg_color")
+            .map(|s| Color::from_str(&s).unwrap_or(Color::Cyan))
+            .unwrap_or(Color::Cyan),
+        level_5: settings
+            .get_string("h5_fg_color")
+            .map(|s| Color::from_str(&s).unwrap_or(Color::Yellow))
+            .unwrap_or(Color::Yellow),
+        level_6: settings
+            .get_string("h6_fg_color")
+            .map(|s| Color::from_str(&s).unwrap_or(Color::LightRed))
+            .unwrap_or(Color::LightRed),
+    }
 }
