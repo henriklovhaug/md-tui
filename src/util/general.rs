474ddc90500c9047958d32c64f2ee9b205eab25e
ancri <andrei.cristea@gmail.com>
2026-09-02 16:44:12 -0400

    Disable the document header by default

--- src/util/general.rs
+++ src/util/general.rs
@@ -38,6 +38,6 @@ pub static GENERAL_CONFIG: LazyLock<GeneralConfig> = LazyLock::new(|| {
             .get::<Centering>("alignment")
             .unwrap_or(Centering::Left),
         help_menu: settings.get::<bool>("help_menu").unwrap_or(true),
-        document_header: settings.get::<bool>("document_header").unwrap_or(true),
+        document_header: settings.get::<bool>("document_header").unwrap_or(false),
     }
 });
